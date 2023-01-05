d4a0ef72a4e23802b13ab101a0c8fa9199b78e47
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2023-01-05 16:47:47 -0500

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ import pandas as pd
 smartsheet_client = smartsheet.Smartsheet("rjjjwNgTfxwAjE5R5YcSKu5OocAMyLAUJa2av")
 sheet = smartsheet_client.Sheets.get_sheet(2954647561365380, 'smartsheet.csv')
 da = []
-f = pd.read_csv('smartsheet.csv')
+f = pd.read_csv(sheet)
 for i in f:
     da.append(i)
 
