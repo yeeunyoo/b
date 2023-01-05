15503d85d6339e00dd1de15ab085206eaf1088fa
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2023-01-05 16:42:09 -0500

    Update home.py

--- home.py
+++ home.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 smartsheet_client = smartsheet.Smartsheet("rjjjwNgTfxwAjE5R5YcSKu5OocAMyLAUJa2av")
 sheet = smartsheet_client.Sheets.get_sheet(2954647561365380)
-
-sheeet = pd.DataFrame(sheet)
+da = []
+for i in sheet:
+    da.append(i)
+sheeet = pd.DataFrame(da)
 st.write(sheeet)
