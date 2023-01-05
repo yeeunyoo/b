5e8b708c534b2a8f8a3f182ae6a796edf3742017
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2023-01-05 16:40:16 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,9 @@
 import streamlit as st   
 import smartsheet
-
+import pandas as pd
 
 smartsheet_client = smartsheet.Smartsheet("rjjjwNgTfxwAjE5R5YcSKu5OocAMyLAUJa2av")
-sheet = smartsheet_client.Sheets.get_sheet(1785827530434436)
-st.write(sheet)
+sheet = smartsheet_client.Sheets.get_sheet(2954647561365380)
+
+sheeet = pd.DataFrame(sheet)
+st.write(sheeet)
